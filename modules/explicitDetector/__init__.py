f4ac7431423c1bc0e8eb807689bc8373d12e981c
Flower Black <36918894+FlowerBlackG@users.noreply.github.com>
2021-09-07 14:21:35 +0800

    Add files via upload

---
+++ modules/explicitDetector/__init__.py
@@ -0,0 +1,58 @@
+"""
+Sayu::Modules::ExplicitDetector
+复读检查模块
+By GTY
+"""
+
+import json
+import os
+import random
+
+from graia.application import Member
+from graia.application.message.elements.internal import At
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+from graia.application.entry import Group, GroupMessage, Plain, MessageChain, GraiaMiraiApplication
+
+# 插件信息
+__name__ = "explicit_detector"
+__description__ = "脏话检测"
+__author__ = "GTY"
+__usage__ = "群内产生消息时，主动处理。"
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name(__name__)
+channel.description(f"{__description__}\n使用方法：{__usage__}")
+channel.author(__author__)
+
+
+def warn_dont_speak_explicitly():
+    msg_list = ['别讲脏话哦..',
+                '不许说脏话..',
+                '文明一点点..',
+                '你不可爱了..']
+    return random.choice(msg_list)
+
+
+exp_list = list()  # 脏话列表
+
+current_path = os.path.dirname(__file__)
+with open(current_path + '/exp_list.json', 'r', encoding='utf-8') as f:
+    exp_list = json.load(f)['data']
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def exp_detector(app: GraiaMiraiApplication, group: Group,
+                         message: MessageChain, member: Member):
+
+    for w in exp_list:
+        if w in message.asDisplay():
+            msg_list = [
+                At(member.id),
+                Plain(' '),
+                Plain(warn_dont_speak_explicitly())
+            ]
+            await app.sendGroupMessage(group, MessageChain.create(msg_list))
