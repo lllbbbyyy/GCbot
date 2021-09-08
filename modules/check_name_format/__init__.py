b64cea1cd2435b72ff6d33ff57c7789b3f18a0b1
Magic843 <89147560+Magic843@users.noreply.github.com>
2021-09-08 22:04:28 +0800

    修改check_name_format模块的细节问题 (#17)
    * 修改细节问题
    
    * 将正则表达式变量改为全局

--- modules/check_name_format/__init__.py
+++ modules/check_name_format/__init__.py
@@ -28,37 +28,42 @@ with open(current_path + '/config.json', 'r', encoding='utf-8') as f:
     config_info = json.load(f)
 
 
+def check_name_format_inner_get_reg_ex():
+    # 判断是否匹配的正则表达式
+    reg_ex = '^' + config_info['stu-years'] + '5[0-9]{4}\-(信(01'
+    # 班号（感觉有点笨）
+    for num in range(2, config_info['class-num'] + 1):
+        reg_ex = reg_ex + '|'
+        if num < 10:
+            reg_ex = reg_ex + '0'
+        reg_ex = reg_ex + str(num)
+    # 专业
+    reg_ex = reg_ex + ')'
+    for major in config_info['major-list']:
+        reg_ex = reg_ex + '|' + major
+    # 姓名
+    reg_ex = reg_ex + ")\-[\u4E00-\u9FA5"
+    for ch in config_info['special-ch']:    # 姓名中可能包含的特殊字符
+        reg_ex = reg_ex + '|' + ch
+    reg_ex = reg_ex + "]+$"
+    return reg_ex
+
+
+reg_ex = check_name_format_inner_get_reg_ex()
+
+
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def check_name_format(app: GraiaMiraiApplication, message: MessageChain,
                             group: Group):
-    msg = [Plain(config_info['top-prompt'])]
-    msg.append(Plain("\n"))
-
+    global reg_ex
     if message.asDisplay() == config_info['ask']:
-        # 判断是否匹配的正则表达式
-        reg_ex = '^' + config_info['stu-years'] + '5[0-9][0-9][0-9][0-9]\-(信(01'
-        # 班号（感觉有点笨）
-        for num in range(2, config_info['class-num'] + 1):
-            reg_ex = reg_ex + '|'
-            if num < 10:
-                reg_ex = reg_ex + '0'
-            reg_ex = reg_ex + str(num)
-        # 专业
-        reg_ex = reg_ex + ')'
-        for major in config_info['major-list']:
-            reg_ex = reg_ex + '|' + major
-        reg_ex = reg_ex + ")\-[\u4E00-\u9FA5]+$"
-
+        msg = [Plain(config_info['top-prompt'])]
+        msg.append(Plain("\n"))    
         memlist = await app.memberList(group)
         for mem in memlist:
-            ignore = 0
             # 白名单检查
-            for white_id in config_info['white-list']:
-                if mem.id == white_id:
-                    ignore = 1
-            if ignore:
+            if mem.id in config_info['white-list']:
                 continue
-
             if not re.match(reg_ex, mem.name):
                 msg.append(At(mem.id))
                 msg.append(Plain("\n"))
