846c0fb8ca249dd8b32c5bdc5d916b4cea1d09d4
Flower Black <36918894+FlowerBlackG@users.noreply.github.com>
2021-09-08 09:13:35 +0800

    Add files via upload

--- modules/explicit_detector/__init__.py
+++ modules/explicit_detector/__init__.py
@@ -8,6 +8,8 @@ import json
 import os
 import random
 
+import requests
+
 from graia.application import Member
 from graia.application.message.elements.internal import At
 from graia.saya import Saya, Channel
@@ -38,33 +40,55 @@ with open(current_path + '/warning_words_list.json', 'r', encoding='utf-8') as f
 def warn_dont_speak_explicitly():
     return random.choice(warning_words_list)
 
+with open(current_path + '/config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
 
-exp_list = list()  # 脏话列表
+if config['should_use_local_exp_list']:
 
-'''
-请将脏话词库以json格式存储于同路径 exp_list.json 文件中
-json格式：
-{
-  "data": [
-    "脏话1",
-    "脏话2",
-    ...
-  ]
-}
-'''
+    exp_list = list()  # 脏话列表
 
-with open(current_path + '/exp_list.json', 'r', encoding='utf-8') as f:
-    exp_list = json.load(f)['data']
+    '''
+    请将脏话词库以json格式存储于同路径 exp_list.json 文件中
+    json格式：
+    {
+      "data": [
+        "脏话1",
+        "脏话2",
+        ...
+      ]
+    }
+    '''
+
+    with open(current_path + config['local_exp_list_dir'], 'r', encoding='utf-8') as f:
+        exp_list = json.load(f)['data']
+
+
+def is_exp_detected(ori: str):
+    if config['should_use_local_exp_list']:  # 使用本地列表
+        for w in exp_list:
+            if w in ori:
+                return True
+        return False
+    else:  # 使用云检测
+        print(f'Sayu::Module::ExplicitDetector: Cloud Detect')
+        url = config['cloud_api']['api']  # api 地址
+
+        # 完整链接。包含字符串参数。
+        full_url = f'{url}?{config["cloud_api"]["str_key"]}="{ori}"'
+
+        response = requests.get(url=full_url)
+        return str(response.text) == '1'
+
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def exp_detector(app: GraiaMiraiApplication, group: Group,
                        message: MessageChain, member: Member):
-    for w in exp_list:
-        if w in message.asDisplay():
-            msg_list = [
-                At(member.id),
-                Plain(' '),
-                Plain(warn_dont_speak_explicitly())
-            ]
-            await app.sendGroupMessage(group, MessageChain.create(msg_list))
+            
+    if is_exp_detected(message.asDisplay()):
+        msg_list = [
+            At(member.id),
+            Plain(' '),
+            Plain(warn_dont_speak_explicitly())
+        ]
+        await app.sendGroupMessage(group, MessageChain.create(msg_list))
--- modules/explicit_detector/readme.md
+++ modules/explicit_detector/readme.md
@@ -1,9 +1,30 @@
 # explicit_detector
 用于检测文本中是否存在敏感词，在检测到敏感词后@该用户，并做出提醒。
 
-模块依赖于两个json文件，存储相关信息。
+### 在 config.json 中设置脏话词库信息：
 
-exp_list.json 负责存储敏感词列表：
+    {
+        "should_use_local_exp_list": false,
+	    "local_exp_list_dir": "/exp_list.json",
+        "cloud_api": {
+            "api": "https://www.yourserver.com/yourloc/api/yourapi.php",
+            "str_key": "str"
+        }
+    }
+
+其中：
+
+`should_use_local_exp_list` 表示是否使用本地脏话词库。
+
+`local_exp_list_dir` 表示如果使用本地词库，词库的文件地址位置。（假设为 **/exp_list.json**）
+
+`cloud_api` 负责存储云接口信息。
+
+其中，`api` 表示接口地址，`str_key` 表示传入参数键。使用 **Get** 请求完成查询。返回值以 **1** 和 **0** 表示是否检测到脏话。
+
+## 模块依赖于两个json文件，存储相关信息。
+
+### exp_list.json 负责存储敏感词列表：
 
     {
         "data": [
@@ -13,7 +34,7 @@ exp_list.json 负责存储敏感词列表：
         ]
     }
 
-warning_words_list.json 负责存储检测到敏感词后，提醒用户的语句：
+### warning_words_list.json 负责存储检测到敏感词后，提醒用户的语句：
 
     [
         "不许说脏话",
