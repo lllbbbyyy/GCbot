213d38c1e6f3ee7c69e5084a0bb96b7c24de3bfe
Magic843 <1318477296@qq.com>
2021-09-09 08:56:49 +0800

    增加权限设置

--- modules/check_name_format/__init__.py
+++ modules/check_name_format/__init__.py
@@ -29,45 +29,54 @@ with open(current_path + '/config.json', 'r', encoding='utf-8') as f:
     config_info = json.load(f)
 
 
-def check_name_format_inner_get_reg_ex():
+def check_name_format_inner_get_reg_ex(strategy):
     # 判断是否匹配的正则表达式
-    reg_ex = '^' + config_info['stu-years'] + '5[0-9]{4}\-(信(01'
+    reg_ex = '^' + strategy['stu-years'] + '5[0-9]{4}\-(信(01'
     # 班号（感觉有点笨）
-    for num in range(2, config_info['class-num'] + 1):
+    for num in range(2, strategy['class-num'] + 1):
         reg_ex = reg_ex + '|'
         if num < 10:
             reg_ex = reg_ex + '0'
         reg_ex = reg_ex + str(num)
     # 专业
     reg_ex = reg_ex + ')'
-    for major in config_info['major-list']:
+    for major in strategy['major-list']:
         reg_ex = reg_ex + '|' + major
     # 姓名
     reg_ex = reg_ex + ")\-[\u4E00-\u9FFF"
-    for ch in config_info['special-ch']:    # 姓名中可能包含的特殊字符
+    for ch in strategy['special-ch']:    # 姓名中可能包含的特殊字符
         reg_ex = reg_ex + '|' + ch
     reg_ex = reg_ex + "]+$"
     return reg_ex
-
-
-reg_ex = check_name_format_inner_get_reg_ex()
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def check_name_format(app: GraiaMiraiApplication, message: MessageChain,
                             group: Group, member: Member):
     global reg_ex
-    if message.asDisplay() == config_info['ask']:
-        if member.permission == MemberPerm.Owner or member.permission == MemberPerm.Administrator:
-            msg = [Plain(config_info['top-prompt'])]
-            msg.append(Plain("\n"))    
-            memlist = await app.memberList(group)
-            for mem in memlist:
-                # 白名单检查
-                if mem.id in config_info['white-list']:
-                    continue
-                if not re.match(reg_ex, mem.name):
-                    msg.append(At(mem.id))
-                    msg.append(Plain("\n"))
-            msg.append(Plain(config_info['bottom-prompt']))
-            await app.sendGroupMessage(group, MessageChain.create(msg))
+    if str(group.id) in config_info['groups-enabled'].keys():
+        # 判断群组权限
+        strategy = config_info['default-strategy']
+        if not config_info['groups-enabled'][str(group.id)]['use-default-strategy']:
+            strategy = config_info['groups-enabled'][str(group.id)]['strategy']
+        if message.asDisplay() == strategy['ask']:
+            # 判断群员权限
+            if strategy['permission'] == 'All' or\
+              (strategy['permission'] == 'Owner-Admin' and member.permission != MemberPerm.Member) or\
+              (strategy['permission'] == 'Owner' and member.permission == MemberPerm.Owner):
+                reg_ex = check_name_format_inner_get_reg_ex(strategy)
+                msg = [Plain(strategy['top-prompt'])]
+                msg.append(Plain("\n"))    
+                memlist = await app.memberList(group)
+                for mem in memlist:
+                    # 白名单检查
+                    if mem.id in strategy['white-list']:
+                        continue
+                    if not re.match(reg_ex, mem.name):
+                        if strategy['should-at']:
+                            msg.append(At(mem.id))
+                        else:
+                            msg.append(Plain(mem.name))
+                        msg.append(Plain("\n"))
+                msg.append(Plain(strategy['bottom-prompt']))
+                await app.sendGroupMessage(group, MessageChain.create(msg))
--- modules/check_name_format/readme.md
+++ modules/check_name_format/readme.md
@@ -6,16 +6,52 @@
 
 ```json
 {
-    "ask" : "检查群名片",
-    "top-prompt" : "请修改群名片:",
-    "bottom-prompt" : "",
-    "white-list" : [278787983],
-    "stu-years" : "(17|18|19|20|21)",
-    "class-num" : 21,
-    "major-list" : ["计拔", "计科", "软工", "信安", "大数据", "通信", "微电子", "测绘"],
-    "special-ch" : ["·"]
+    "default-strategy" :
+    {
+        "ask" : "检查群名片",
+        "top-prompt" : "请修改群名片:",
+        "bottom-prompt" : "",
+        "white-list" : [278787983],
+        "stu-years" : "(17|18|19|20|21)",
+        "class-num" : 21,
+        "major-list" : ["计拔", "计科", "软工", "信安", "大数据", "通信", "微电子", "测绘"],
+        "special-ch" : ["·"],
+        "permission" : "Owner-Admin",
+        "should-at" : true
+    },
+    
+    "groups-enabled" :
+    {
+        "642362943" :
+        {
+            "use-default-strategy" : true
+        },
+        "123456789" :
+        {
+            "use-default-strategy" : false,
+            "strategy" :
+            {
+                "ask" : "test",
+                "top-prompt" : "测试用，不@人",
+                "bottom-prompt" : "",
+                "white-list" : [278787983],
+                "stu-years" : "(17|18|19|20|21)",
+                "class-num" : 21,
+                "major-list" : ["计拔", "计科", "软工", "信安", "大数据", "通信", "微电子", "测绘"],
+                "special-ch" : ["·"],
+                "permission" : "All",
+                "should-at" : false
+            }
+        }
+    }
 }
 ```
+
+`default-strategy`记录默认处理方式
+
+应将需要启用本插件的群号作为键值添加到`group-enabled`中，内容为一个 json_object ，记录策略等信息。如果 `use-default-strategy`内容为 true，则在该群加载默认策略。
+
+对于策略的说明：
 
 字符串`ask`表示检测的询问语句
 字符串`top-prompt`表示机器人发出的消息的头部提示信息
@@ -25,6 +61,8 @@
 数`class-num`表示本届信息类班级个数
 字符串`major-list`表示转专业补课同学都来自哪些专业
 列表`special-ch`表示学生姓名中可能包含的特殊符号
+字符串`permission`表示触发该插件功能所需要的权限，有三种：`Owner`, `Owner-Admin`, `All`
+`should-at`表示发出信息时是否@群名片不正确的成员
 
 当在**群聊**中出现`ask`语句时，机器人回复信息格式如下:
 
